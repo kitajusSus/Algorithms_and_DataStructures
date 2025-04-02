53dc721a6f5b7e070a89241528067ecb6aaa2e07
kitajuSus <kitaj2458@gmail.com>
2025-04-02 22:28:04 +0200

    cpp zaj6

---
+++ zaj6/linked_list.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+// samoreferecnyjna lista 
+class Link{
+  public :
+    int data;
+    double dData;
+    Link* pNext;
+};
+// to jest to samo ale inaczej,  inventory item to jest wskaźnik do obiektu przechowującego dane
+/*
+class Link{
+  public; 
+    inventoryItem* pIteam;
+    Link* pNext;
+};
+*/
+
+
+
