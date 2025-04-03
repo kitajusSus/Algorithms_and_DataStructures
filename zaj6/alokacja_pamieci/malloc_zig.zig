a5d60a190ac40fea28ee053bc26741f939e793a1
kitajuSus <kitaj2458@gmail.com>
2025-04-03 17:26:04 +0200

    ls

--- zaj6/alokacja_pamieci/malloc_zig.zig
+++ zaj6/alokacja_pamieci/malloc_zig.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    var arr = try allocator.alloc(u32, 5);
+    const arr = try allocator.alloc(u32, 5);
     defer allocator.free(arr);
 
     for (arr, 0..) |*item, index| {
@@ -15,3 +15,18 @@ pub fn main() !void {
     }
     std.debug.print("\n", .{});
 }
+
+//W Zig, w pętli `for` nad tablicą, możesz iterować na dwa sposoby:
+
+//1. **Jednym elementem**: Jeśli używasz tylko jednej zmiennej, np. `|item|`, to iterujesz po wartościach elementów tablicy.
+//2. **Dwoma elementami**: Jeśli używasz dwóch zmiennych, np. `|*item, index|`, to:
+ //  - `*item` to wskaźnik do elementu tablicy (możesz modyfikować wartość).
+//   - `index` to indeks aktualnego elementu w tablicy.
+
+//W kodzie:
+//- Pierwsza pętla `for (arr) |*item, index|` używa wskaźnika `*item` do modyfikacji wartości w tablicy oraz `index` do przypisania wartości zależnej od indeksu.
+//- Druga pętla `for (arr) |item|` iteruje tylko po wartościach elementów tablicy, aby je wypisać.
+
+//To pozwala na różne operacje w zależności od potrzeb: modyfikację w pierwszej pętli i odczyt w drugiej.
+
+
