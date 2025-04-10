3a6aad6da613e1faf45b41e9d17b3afacecba302
skibidier <you@example.com>
2025-04-10 08:18:50 +0200

    s

--- zaj6/alokacja_pamieci/malloc_zig.zig
+++ zaj6/alokacja_pamieci/malloc_zig.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    var arr = try allocator.alloc(u32, 5);
+    const arr = try allocator.alloc(u32, 5);
     defer allocator.free(arr);
 
     for (arr, 0..) |*item, index| {
