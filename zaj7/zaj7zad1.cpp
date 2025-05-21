6785514affe3845edc393c85b8c8fe169cc7ee60
kitajuSus <kitaj2458@gmail.com>
2025-05-21 20:47:27 +0200

    s

--- zaj7/zaj7zad1.cpp
+++ zaj7/zaj7zad1.cpp
@@ -6,20 +6,20 @@ using namespace std;
 void znajdzAnagramy(string slowo, int poczatek, int koniec);
 
 int main() {
-    string slowo = "skibidi";
-    znajdzAnagramy(slowo, 0, slowo.length() - 1);
-    return 0;
+  string slowo = "skibidi";
+  znajdzAnagramy(slowo, 0, slowo.length() - 1);
+  return 0;
 }
 
 void znajdzAnagramy(string slowo, int poczatek, int koniec) {
-    if (poczatek == koniec) {
-        cout << slowo << endl;
-        return;
-    }
-    
-    for (int i = poczatek; i <= koniec; i++) {
-        swap(slowo[poczatek], slowo[i]);
-        znajdzAnagramy(slowo, poczatek + 1, koniec);
-        swap(slowo[poczatek], slowo[i]);
-    }
+  if (poczatek == koniec) {
+    cout << slowo << endl;
+    return;
+  }
+
+  for (int i = poczatek; i <= koniec; i++) {
+    swap(slowo[poczatek], slowo[i]);
+    znajdzAnagramy(slowo, poczatek + 1, koniec);
+    swap(slowo[poczatek], slowo[i]);
+  }
 }
